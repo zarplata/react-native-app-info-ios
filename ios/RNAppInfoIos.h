267c9fd4c444b25a00601ce645af4cd0cf0e4984
Dmitriy Arkhipov <d.arkhipov@office.ngs.ru>
2017-09-22 09:51:59 +0700

    Удален неиспользуемый импорт RCTEventEmitter

--- ios/RNAppInfoIos.h
+++ ios/RNAppInfoIos.h
@@ -8,10 +8,8 @@
 
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
-#import "RCTEventEmitter.h"
 #else
 #import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
 #endif
 
 @interface RNAppInfoIos : NSObject <RCTBridgeModule>
